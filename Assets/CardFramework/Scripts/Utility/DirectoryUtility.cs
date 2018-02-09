afd68ab58c6f02591bd8ce14a5b209fe5acaef09
CrandellWS <william@crandell.ws>
2018-02-08 21:33:29 -0500

    DirectoryUtility StreamingAsses

--- Assets/CardFramework/Scripts/Utility/DirectoryUtility.cs
+++ Assets/CardFramework/Scripts/Utility/DirectoryUtility.cs
@@ -6,17 +6,18 @@ static public class DirectoryUtility
 {
 	static public string ExternalAssets()
 	{
-		switch ( Application.platform )
-		{
-		default:
-		case RuntimePlatform.OSXEditor:
-		case RuntimePlatform.WindowsEditor:	
-			return Application.dataPath + "/../ExternalAssets/";			
-		case RuntimePlatform.OSXPlayer:
-		case RuntimePlatform.WindowsPlayer:
-		case RuntimePlatform.IPhonePlayer:		
-		case RuntimePlatform.LinuxPlayer:
-			return Application.dataPath + "/ExternalAssets/";		
-		}	
+//		switch ( Application.platform )
+//		{
+//		default:
+			return Application.streamingAssetsPath;
+//		case RuntimePlatform.OSXEditor:
+//		case RuntimePlatform.WindowsEditor:	
+//			return Application.dataPath + "/../ExternalAssets/";			
+//		case RuntimePlatform.OSXPlayer:
+//		case RuntimePlatform.WindowsPlayer:
+//		case RuntimePlatform.IPhonePlayer:		
+//		case RuntimePlatform.LinuxPlayer:
+//			return Application.dataPath + "/ExternalAssets/";		
+//		}	
 	}  
 }
